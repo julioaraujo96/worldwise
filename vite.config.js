372b4946dbb664856011f7214441cd8a726117bd
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-08-10 15:40:43 +0100

    feat: config alias for paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
+
+// Derive the directory path from import.meta.url
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+       resolve: {
+       alias: {
+         '@components': path.resolve(__dirname, 'src/components'),
+         '@pages': path.resolve(__dirname, 'src/pages'),
+       },
+     },
 })
