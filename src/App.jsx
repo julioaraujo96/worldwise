838684a7d1038b582c6f18449864f6aceafac92d
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-08-10 20:53:33 +0100

    feat: sub navigation / nested route

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
       <Route path="product" element={<Product/>} />
       <Route path="pricing" element={ <Pricing/> } />
       <Route path="login" element={ <Login /> } />
-      <Route path="app" element={ <AppLayout /> } />
+      <Route path="app" element={ <AppLayout /> }>
+          <Route index element={ <Homepage /> } /> 
+          <Route path="cities" element={ <Homepage /> } />
+          <Route path="countries" element={ <Homepage /> } />
+          <Route path="form" element={ <Homepage /> } />
+      </Route>
       <Route path="*" element={ <PageNotFound /> } />
     </Routes> 
   </BrowserRouter>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import AppNav from '../AppNav/AppNav';
-import styles from './Sidebar.module.css'
-
+import styles from './Sidebar.module.css';
 
 function Sidebar() {
-    return (
-        <div className={styles.sidebar}>
-           <Logo />
-           <AppNav /> 
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
 
-           <p>List of cities</p>
-           <footer className={styles.footer}>
-                <p className={styles.copyright}>
-                    &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-                </p>
-           </footer>
-        </div>
-    )
+      <Outlet />
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
