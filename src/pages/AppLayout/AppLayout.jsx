da378d3e5fe43636f8d0a02f9965419f94dabc88
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-08-10 20:31:24 +0100

    page styling

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,12 +1,14 @@
-import AppNav from "../../components/AppNav/AppNav";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Map from '../../components/Map/Map';
+import styles from './AppLayout.module.css';
 
 function AppLayout() {
     return (
-        <main>
-            <AppNav />
-            <p>App</p>
-        </main>
+       <div className={styles.app} >
+            <Sidebar />
+            <Map />
+        </div>
     )
 }
 
-export default AppLayout
+export default AppLayout;
