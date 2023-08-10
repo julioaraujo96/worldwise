11b44680324c0685048d548278c103f6703077d7
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-08-10 15:26:38 +0100

    building app pages

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+    return (
+        <main>
+            <AppNav />
+            <p>App</p>
+        </main>
+    )
+}
+
+export default AppLayout
